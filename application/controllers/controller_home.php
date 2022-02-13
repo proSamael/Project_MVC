a76585a73aa4e3f5c3cb7cc86f39d6f7001b423a
proSamael <pro100.ink@gmail.com>
2022-02-13 16:37:01 +0300

    add logon page.

---
+++ application/controllers/controller_home.php
@@ -0,0 +1,36 @@
+<?php
+
+class Controller_home extends Controller
+{
+
+    function action_index()
+    {
+        session_start();
+
+        /*
+        Для простоты, в нашем случае, проверяется равенство сессионной переменной admin прописанному
+        в коде значению — паролю. Такое решение не правильно с точки зрения безопасности.
+        Пароль должен храниться в базе данных в захешированном виде, но пока оставим как есть.
+        */
+        if ( $_SESSION['auth'] == true )
+        {
+            var_dump($_SESSION);
+            $this->view->generate('home_view.php', 'template_view.php');
+        }
+        else
+        {
+            session_destroy();
+            Route::ErrorPage404();
+        }
+
+    }
+
+    // Действие для разлогинивания администратора
+    function action_logout()
+    {
+        session_start();
+        session_destroy();
+        header('Location:./');
+    }
+
+}
