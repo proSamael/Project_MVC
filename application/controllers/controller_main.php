e02284bb991c0ab07bf47109dcfb49d54412f2f7
proSamael <pro100.ink@gmail.com>
2022-02-13 16:39:32 +0300

    add check auth and redirection

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -2,9 +2,18 @@
 
 class Controller_Main extends Controller
 {
-
 	function action_index()
-	{	
+	{
+        //так как сайта не будет делаем сразу переадрисацию на авторизацию в случае отсуствия сессии
+        if(isset($_SESSION['auth'])) {
+            if ($_SESSION['auth'] == true) {
+                header('Location:./index.php?home');
+            }else{
+                header('Location:./index.php?login');
+            }
+        }else{
+            header('Location:./index.php?login');
+        }
 		$this->view->generate('main_view.php', 'template_view.php');
 	}
 }
