86ad988a98ff68f3317369954f9c68267814a506
proSamael <pro100.ink@gmail.com>
2022-02-20 19:58:14 +0300

    Авторизация по логину или email. Функция Запомнить меня по токену авторизации+куки.

--- application/controllers/controller_home.php
+++ application/controllers/controller_home.php
@@ -2,11 +2,24 @@
 
 class Controller_home extends Controller
 {
+    private $db;
+
+    function __construct()
+    {
+        $this->view = new View();
+        $this->db = new Database();
+    }
 
     function action_index()
     {
         session_start();
-
+        /* Проверяем существует ли токен авторизации если существуем доступ разрешен */
+        if(isset($_COOKIE["password_cookie_token"])){
+            $token_pass = $this->db->getRow('SELECT * FROM users WHERE token = ?s LIMIT 0,1', $_COOKIE["password_cookie_token"]);
+            if(isset($token_pass) && ($token_pass != false)){
+                $_SESSION['auth'] = true;
+            }
+        }
         /*
         Для простоты, в нашем случае, проверяется равенство сессионной переменной auth прописанному
         в коде значению — true.
@@ -20,14 +33,16 @@ class Controller_home extends Controller
             session_destroy();
             Route::logout();
         }
-
     }
 
-    // Действие для разлогинивания администратора
+    // Действие для разлогинивания
     function action_logout()
     {
         session_start();
+        // Удляем сейссию
         session_destroy();
+        //Удаляем куку password_cookie_token
+        setcookie("password_cookie_token", "", time() - 3600);
         header('Location:./index.php?login');
     }
 
